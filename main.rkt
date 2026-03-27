fbfa5a31b7346575578954b0b05bf2063af0fb50
Kunihisa Matsuda <love_coke@mac.com>
2026-03-27 21:08:14 +0900

    little repl

--- main.rkt
+++ main.rkt
@@ -2,27 +2,46 @@
 (module+ test
   (require rackunit))
 
-(require "hello.rkt")
+(define (read-expr)
+  (display "> ")
+  (read))
 
-(provide hello-world)
+(define (eval-expr expr)
+    (displayln expr))
 
-(define (hello-world)
-  (displayln "Hello, World!"))
+(define (quit? expr)
+  (or (eq? expr 'quit)
+      (eq? expr 'q)))
 
+(define (bye)
+  (displayln "bye."))
+
+(define (repl)
+  (let ([expr (read-expr)])
+    (cond [(quit? expr) (bye)]
+          [else
+            (eval-expr expr)
+            (repl)])))
 
 (module+ test
   ;; Any code in this `test` submodule runs when this file is run using DrRacket
   ;; or with `raco test`. The code here does not run when this file is
   ;; required by another module.
 
-  (check-equal? (+ 2 2) 4))
+  ;quit? test
+  (check-equal? (quit? 'quit) #t)
+  (check-equal? (quit? 'q) #t)
+  (check-equal? (quit? 'hello) #f)
+
+)
 
 (module+ main
   ;; (Optional) main submodule. Put code here if you need it to be executed when
   ;; this file is run using DrRacket or the `racket` executable.  The code here
   ;; does not run when this file is required by another module. Documentation:
   ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
+  (repl)
+  #|
   (require racket/cmdline)
   (define who (box "world"))
   (command-line
@@ -31,4 +50,5 @@
     [("-n" "--name") name "Who to say hello to" (set-box! who name)]
     #:args ()
     (printf "hello ~a~n" (unbox who))))
-
+|#
+)
